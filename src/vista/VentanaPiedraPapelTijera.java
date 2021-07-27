5c9d2b0a50274fe0d6c80f68d47ac113a2cfcf20
AlfredoCalle <alfrcall@espol.edu.ec>
2021-07-27 12:42:15 -0500

    Retoques en la interfaz

--- src/vista/VentanaPiedraPapelTijera.java
+++ src/vista/VentanaPiedraPapelTijera.java
@@ -135,7 +135,7 @@ public class VentanaPiedraPapelTijera extends javax.swing.JFrame {
                 botonReiniciarActionPerformed(evt);
             }
         });
-        jPanel1.add(botonReiniciar, new org.netbeans.lib.awtextra.AbsoluteConstraints(580, 0, 100, 30));
+        jPanel1.add(botonReiniciar, new org.netbeans.lib.awtextra.AbsoluteConstraints(590, 0, 100, 30));
 
         txtGanador.setFont(new java.awt.Font("Chiller", 1, 36)); // NOI18N
         txtGanador.setForeground(new java.awt.Color(255, 255, 255));
@@ -161,7 +161,7 @@ public class VentanaPiedraPapelTijera extends javax.swing.JFrame {
         jLabel5.setText("Creador: Alfredo Calle Aguilar");
         jPanel1.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 410, 180, 30));
 
-        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, -2, 680, 440));
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, -2, 690, 440));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -202,7 +202,37 @@ public class VentanaPiedraPapelTijera extends javax.swing.JFrame {
     }
     */
     
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(VentanaPiedraPapelTijera.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(VentanaPiedraPapelTijera.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(VentanaPiedraPapelTijera.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(VentanaPiedraPapelTijera.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
 
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new VentanaPiedraPapelTijera().setVisible(true);
+            }
+        });
+    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton botonJugador;
